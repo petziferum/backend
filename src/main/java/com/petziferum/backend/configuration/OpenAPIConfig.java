90f671c2e52b338ca82363e005a7e77035199181
Petzi <daboarderpjb@gmail.com>
2021-03-31 12:49:00 +0200

    add OpenAPIConfig.java

--- src/main/java/com/petziferum/backend/configuration/OpenAPIConfig.java
+++ src/main/java/com/petziferum/backend/configuration/OpenAPIConfig.java
@@ -1,25 +1,28 @@
 package com.petziferum.backend.configuration;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class OpenAPIConfig {
 
-
+    @Bean
     public OpenAPI customOpenAPI(){
      return new OpenAPI()
+             .components(new Components())
              .info(new Info()
-             .title("Petzis OpenAPI 3.0 Backend")
-             .version("0.1.0")
-                     .description("Eine Spielwiese")
+                     .version("1.0.0")
+                     .title("Petzis OpenAPI 3.0 Backend LOLOLOLOLLLOOOOOLL")
+                     .description("Eine Spielwiese. Mehr Infos unter https://swagger.io/specification/#info-version")
                      .termsOfService(null)
-                     .contact(new Contact()
-                     .email("daboarderpjb@gmail.com")
-                     .name("Patrick Bussler"))
+                     .contact(new Contact().name("Patrick Bussler").url("http://www.patrickbussler.de").email("daboarderpjb@gmail.com")
+                     )
                      .license(null)
+
              );
     }
 }
