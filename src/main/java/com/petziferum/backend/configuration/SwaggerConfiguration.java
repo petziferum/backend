cb8de501884edcaaca84c44ddc50838f3896a1ce
Petzi <daboarderpjb@gmail.com>
2021-05-27 13:17:44 +0200

    add environment Variables from application.yaml

--- src/main/java/com/petziferum/backend/configuration/SwaggerConfiguration.java
+++ src/main/java/com/petziferum/backend/configuration/SwaggerConfiguration.java
@@ -3,6 +3,7 @@ package com.petziferum.backend.configuration;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,12 +11,12 @@ import org.springframework.context.annotation.Configuration;
 public class SwaggerConfiguration {
 
     @Bean
-    public OpenAPI customOpenAPI(){
+    public OpenAPI customOpenAPI(@Value("${spring.application.name}") String appName){
      return new OpenAPI()
              .openapi("3.0.1")
              .info(new Info()
                      .version("1.0.0")
-                     .title("Straßen Backend Service")
+                     .title(appName)
                      .description("Ein Backend Service für das Straßen Verwaltungs-Tool")
                      .contact(new Contact()
                              .name("Max Mustermann")
